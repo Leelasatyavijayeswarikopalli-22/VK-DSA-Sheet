8c433fb971f42cac539a78ccc6e23dcbe2f07f01
Leelasatyavijayeswarikopalli-22 <leelasatyavijayeswari1022@gmail.com>
2026-03-26 02:18:26 +0530

    Changed server.js

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "frontend")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "index.html"));
+    res.sendFile(path.join(__dirname, "Frontend", "index.html"));
 });
 
 console.log("🔹 Server script running...");
